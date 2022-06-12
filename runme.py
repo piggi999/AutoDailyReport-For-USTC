9c451fabe13921427d5fe891ff4d64f7b16aa875
piggi999 <87293723+piggi999@users.noreply.github.com>
2022-06-12 20:10:33 +0800

    Update runme.py

--- runme.py
+++ runme.py
@@ -2,9 +2,9 @@ import argparse
 from Report import Report
 
 report_data = [
-    ("juzhudi", "中校区"),  # 居住地
-    ("dorm_building", "z2"),  # 宿舍楼
-    ("dorm", "414"),  # 宿舍号
+    ("juzhudi", "高新校区"),  # 居住地
+    ("dorm_building", "g002"),  # 宿舍楼
+    ("dorm", "305"),  # 宿舍号
     ("body_condition", "1"),  # 身体状况 1为正常
     ("body_condition_detail", ""),  # 身体状况详细 正常无需
     ("now_status", "1"),  # 当前状态 1为正常在校园内
@@ -15,24 +15,24 @@ report_data = [
     ("last_touch_sars_detail", ""),  # 具体情况
     ("is_danger", "0"),  # 当前居住地是否为疫情中高风险地区 0为否
     ("is_goto_danger", "0"),  # 14天内是否有疫情中高风险地区旅居史 0为否
-    ("jinji_lxr", "cyzkrau"),  # 紧急联系人
-    ("jinji_guanxi", "self"),  # 与本人关系
-    ("jiji_mobile", "18701375065"),  # 紧急联系人电话
-    ("other_detail", ""),  # 其他情况说明
+    ("jinji_lxr", "孙建利"),  # 紧急联系人
+    ("jinji_guanxi", "父子"),  # 与本人关系
+    ("jiji_mobile", "17318588076"),  # 紧急联系人电话
+    ("other_detail", "无"),  # 其他情况说明
 ]
 cross_campus_data = [
-    ("return_college[]", "东校区"),  # 往返校区
+#     ("return_college[]", "东校区"),  # 往返校区
     ("return_college[]", "西校区"),  # 往返校区
-    ("return_college[]", "南校区"),  # 往返校区
-    ("return_college[]", "北校区"),  # 往返校区
+#     ("return_college[]", "南校区"),  # 往返校区
+#     ("return_college[]", "北校区"),  # 往返校区
     ("return_college[]", "中校区"),  # 往返校区
-    ("reason", "上课"),  # 原因
+    ("reason", "玩"),  # 原因
 ]
 out_school_data = [
     ("return_college[]", "蜀山区"),  # 目的地
-    ("return_college[]", "包河区"),  # 目的地
-    ("return_college[]", "瑶海区"),  # 目的地
-    ("return_college[]", "庐阳区"),  # 目的地
+#     ("return_college[]", "包河区"),  # 目的地
+#     ("return_college[]", "瑶海区"),  # 目的地
+#     ("return_college[]", "庐阳区"),  # 目的地
     ("reason", "玩"),  # 原因
 ]
 if __name__ == "__main__":
